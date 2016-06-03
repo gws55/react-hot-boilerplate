322613705c0da99b4e667ce29600394fd60de6f4
gws55 <gws55@cornell.edu>
2016-06-03 12:35:24 -0400

    refactored again

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, browserHistory } from 'react-router';
-import routes from '../routes';
+import routes from './routes';
 
 class App extends React.Component {
   render() {
